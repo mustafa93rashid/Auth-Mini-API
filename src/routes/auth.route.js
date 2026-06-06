7e77a475cb3b2128ffd1651b2889c0042806c78a
Mustafa Rashid <mustafa.rashid.abbas@gmail.com>
2026-06-06 14:29:32 +0300

    feat: add rate limiting middleware for API protection

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const asyncHandler = require("../utils/asyncHandler");
 const authController = require("../controllers/auth.controller");   
 const auth = require("../middlewares/auth");
+const { signupLimiter, loginLimiter } = require("../middlewares/limiter");
 
-router.post("/register", asyncHandler(authController.register));
-router.post("/login", asyncHandler(authController.login));
+router.post("/register", [signupLimiter], asyncHandler(authController.register));
+router.post("/login", [loginLimiter], asyncHandler(authController.login));
 router.post("/logout", [auth], asyncHandler(authController.logout));
 
 module.exports = router;
